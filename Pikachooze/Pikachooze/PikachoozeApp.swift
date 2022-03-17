388f2adb93b32c67e9669119c9176a25a08273d6
Michelle Mao <mm738@duke.edu>
2022-03-17 17:23:14 -0400

    First commit, creating project

---
+++ Pikachooze/Pikachooze/PikachoozeApp.swift
@@ -0,0 +1,17 @@
+//
+//  PikachoozeApp.swift
+//  Pikachooze
+//
+//  Created by student on 3/17/22.
+//
+
+import SwiftUI
+
+@main
+struct PikachoozeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
