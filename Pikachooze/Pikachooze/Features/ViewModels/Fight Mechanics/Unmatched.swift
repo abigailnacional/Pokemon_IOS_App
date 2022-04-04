07b36a7892591793e05dc12df6e3830403c7928f
Abigail Nacional <aan16@duke.edu>
2022-04-04 18:57:41 -0400

    Added Unmatched, which contains functions that handle Pokemon that aren't immediately matched while calculating the suggested Pokemon list

---
+++ Pikachooze/Pikachooze/Features/ViewModels/Fight Mechanics/Unmatched.swift
@@ -0,0 +1,40 @@
+//
+//  Unmatched.swift
+//  Pikachooze
+//
+//  Created by student on 3/31/22.
+//
+// Finds the unmatched Pokemon for the suggest function.
+
+import Foundation
+import Combine
+
+extension FightView {
+    func noMatchPlayPoke() -> [Pokemon] {
+        var ret: [Pokemon] = []
+        //Find all player Pokemon who don't yet have a match
+        for i in 0..<inventoryPokemon.count {
+            if (assign[i] == -1){
+                //Add this Pokemon to the list of unmatched player Pokemon
+                ret.append(inventoryPokemon[i])
+                //Add these indices to invToUnmatched to keep track
+                invToUnmatched.insert((ret.count - 1), at: i)
+            }
+        }
+        return ret
+    }
+    
+    func noMatchGymPoke() -> [Pokemon] {
+        var ret: [Pokemon] = []
+        //Find all gym leaderPokemon who don't yet have a match
+        for i in 0..<gymLeader.gymPokemon.count {
+            if (!assign.contains(i)){
+                //Add this Pokemon to the list of unmatched gym leader Pokemon
+                ret.append(gymLeader.gymPokemon[i])
+                //Add these indices to invToUnmatched to keep track
+                glToUnmatched.insert((ret.count - 1), at: i)
+            }
+        }
+        return ret
+    }
+}
