a2cbc5ee9c6edde27c8a61e2a6a5254aac052824
Abby Nacional <abigailnacional@gmail.com>
2022-04-08 01:47:30 -0400

    Added code to put all the extra inventory Pokemon in the suggested order as well

--- Pikachooze/Pikachooze/Features/ViewModels/Fight Mechanics/Suggested.swift
+++ Pikachooze/Pikachooze/Features/ViewModels/Fight Mechanics/Suggested.swift
@@ -41,6 +41,10 @@ extension FightView {
             let unmatchedGl: [Pokemon] = noMatchGymPoke()
             //Find matchings using neutral relationships
             let neutralMatch: Int = maxMatchLeftover(unmatchedPlay, unmatchedGl)
+            
+            print("Unmatched Player Pokemon : \(unmatchedPlay)")
+            print("Unmatched Gym Leader Pokemon : \(unmatchedGl)")
+            print("neutralMatch: \(neutralMatch)")
             if (neutralMatch > 0){
                 //Some Pokemon matched, so use the assigned values as the suggested Pokemon matches
                 // For every gym leader Pokemon
@@ -63,11 +67,30 @@ extension FightView {
                 print("Unmatched Player Pokemon : \(unmatchedPlay2)")
                 print("Unmatched Gym Leader Pokemon : \(unmatchedGl2)")
                 print("glToUnmatched : \(glToUnmatched)")
+                //Iterate through all unmatched gym leader Pokemon
                 for pokeNum in 0..<unmatchedGl2.count {
+                    //Find the actual index of the gym leader Pokemon
                     if let realGlIndex = glToUnmatched.firstIndex(where: {$0 == pokeNum}) {
+                        //Match the first unmatched player Pokemon with the first unmatched gym leader Pokemon
                         suggest[realGlIndex] = unmatchedPlay2[pokeNum]
+                        //Update assign since we haven't used the function that usually updates it
+                        if let realPlayIndex = invToUnmatched.firstIndex(where: {$0 == pokeNum}) {
+                            assign[realPlayIndex] = realGlIndex
+                        }
                     }
                 }
+            }
+        }
+        //If there were more Pokemon in the inventory than gym leader Pokemon, tack the extras onto the end
+        if (inventoryPokemon.count > gymLeader.gymPokemon.count){
+            //Get all the inventory Pokemon that didn't get matched
+            let unmatchedPlay3: [Pokemon] = noMatchPlayPoke()
+            print("Unmatched Player Pokemon (Last) : \(unmatchedPlay3)")
+            //Attach them to the end of suggest
+            var extraPokeIndex = gymLeader.gymPokemon.count
+            for unm3 in unmatchedPlay3 {
+                suggest[extraPokeIndex] = unm3
+                extraPokeIndex += 1
             }
         }
         print("Final Suggest: \(suggest)")
